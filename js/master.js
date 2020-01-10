374e022eb0c5a602dd60b86bae16518e04d05996
frecklebars <popaalex440@gmail.com>
2020-01-10 16:15:13 +0200

    im sorry ill get to actually fixing it some day

--- js/master.js
+++ js/master.js
@@ -1,6 +1,6 @@
 
 function endcontact(){
-	document.getElementById("endcontact").innerHTML='<div class="icons"><a href="https://twitter.com/frecklebars"><img src="../images/icons/twitter.svg"></a>/<a href="https://github.com/frecklebars"><img src="../images/icons/github.svg"></a>/<a href="https://www.instagram.com/frecklebars/"><img src="../images/icons/instagram.svg"></a>/<a href="https://rateyourmusic.com/~frecklebars"><img src="../images/icons/cd.svg"></a></div><div class="text-section"><h3>Sue Me</h3><br><div class="contact"><p>Alexandru Popa, 18</p><p>popaalex440@gmail.com</p></div></div><p class="text-section contact" style="position: absolute; bottom: 15px;">Copyright (c) 2019 Alexandru Popa, "frecklebars"</p>'
+	document.getElementById("endcontact").innerHTML='<div class="icons"><a href="https://twitter.com/frecklebars"><img src="../images/icons/twitter.svg"></a>/<a href="https://github.com/frecklebars"><img src="../images/icons/github.svg"></a>/<a href="https://www.instagram.com/frecklebars/"><img src="../images/icons/instagram.svg"></a>/<a href="https://rateyourmusic.com/~frecklebars"><img src="../images/icons/cd.svg"></a></div><div class="text-section"><h3>Sue Me</h3><br><div class="contact"><p>Alexandru Popa, 18</p><p>popaalex440@gmail.com</p></div></div><p class="text-section contact" style="position: absolute; bottom: 15px;">Copyright (c) 2019-2020 Alexandru Popa, "frecklebars"</p>'
 	// this line is horrid and im sorry, 
 	// i truly am very sorry
 }
