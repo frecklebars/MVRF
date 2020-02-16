c74829ce2ed3fc40de2f968bb5125683548773e6
frecklebars <popaalex440@gmail.com>
2020-02-16 17:50:35 +0200

    new face

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var displayMode = "block";
+
+function displayMenu(id){
+	if(id.startsWith("i")){
+		displayMode = "flex";
+	}
+	id = "menu" + id;
+	var elemDisplayState =  document.getElementById(id).style.display;
+	if(elemDisplayState == "" || elemDisplayState == "none"){
+		document.getElementById(id).style.display = displayMode;
+	}
+	else{
+		document.getElementById(id).style.display = "none";
+	}
+}
