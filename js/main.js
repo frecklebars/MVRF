87b48fe83a6cd80c7400314fdcdbfed325e7f777
frecklebars <popaalex440@gmail.com>
2020-04-01 15:55:46 +0300

    update contact to be better yes

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ function displayMenu(id){
 	}
 }
 
+function ccardsetup(n){
+	backPath = "";
+	if(n){
+		for(i = 0; i<n; i++){
+			backPath += "../";
+		}
+	}
+	var ccard = "";
+	ccard += '<p>Alexandru Popa, 19</p>';
+	ccard += '		<p>aka. "frecklebars", "vaudevian"</p>';
+	ccard += '		<p>popaalex@gmail.com</p><br> ';
+	ccard += '		<div class="icons"> ';
+	ccard += '			<a href="https://github.com/frecklebars"><img class="icon" src="'+ backPath + 'style/icons/github.svg"></a> / ';
+	ccard += '			<a href="https://twitter.com/frecklebars"><img class="icon" src="'+ backPath + 'style/icons/twitter.svg"></a> / ';
+	ccard += '			<a href="https://www.instagram.com/frecklebars/"><img class="icon" src="'+ backPath + 'style/icons/instagram.svg"></a> / ';
+	ccard += '			<a href="https://www.instagram.com/vaudevian/"><img class="icon" src="'+ backPath + 'style/icons/instagram.svg"></a> / ';
+	ccard += '			<a href="https://open.spotify.com/user/3cxkyoe0z7lja736z4zi0cmyl?si=ka2y4_CdSByctSKJauIzNg"><img class="icon" src="'+ backPath + 'style/icons/spotify.svg"></a> / ';
+	ccard += '			<a href="https://www.facebook.com/alex.popa.376043"><img class="icon" src="'+ backPath + 'style/icons/facebook.svg"></a> / ';
+	ccard += '			<a href="https://rateyourmusic.com/~frecklebars"><img class="icon" src="'+ backPath + 'style/icons/cd.svg"></a>';
+	ccard += '		</div>';
+	ccard += '		<br><br>';
+	ccard += '		<p>copyright © 2019-2020, Alexandru Popa</p><br>';
+	document.getElementById("contactcard").innerHTML = ccard;
+}
+
